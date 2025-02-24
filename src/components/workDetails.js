a117814004f372d094536acddd17f416dbd45f90
Elvis-rozy <elvisrozy52@gmail.com>
2025-02-24 12:57:00 +0100

    porject details included

--- src/components/workDetails.js
+++ src/components/workDetails.js
@@ -11,19 +11,18 @@ const WorkDetails =({project, stat, setStat})=>{
         <p>{project.year}</p>
       </div>
       <p className="description a">{project.mainDesc}</p>
-      <p className="description b">{project.mainDesc}</p>
       <div className="views">
-        <img className="screenshot c" src={project.desktopImg} alt={project.name}/>
-        <img className="screenshot d" src={project.mobileImg} alt={project.name}/>
+        <img className="screenshot b" src={project.desktopImg} alt={project.name}/>
+        <img className="screenshot c" src={project.mobileImg} alt={project.name}/>
       </div>
       <div className="techStack">
+        <img className="d" src="./media/icons/figma.png" alt=""/>
         <img className="e" src="./media/icons/figma.png" alt=""/>
         <img className="f" src="./media/icons/figma.png" alt=""/>
-        <img className="g" src="./media/icons/figma.png" alt=""/>
       </div>
       <div className="projectLinks">
-        <a className="button h" href={project.codeLink}>View Github Code</a>
-        <a className="button i" href={project.webLink}>View Live Website</a>
+        <a className="button g" href={project.codeLink}>View Github Code</a>
+        <a className="button h" href={project.webLink}>View Live Website</a>
       </div>
     </div>
   )
